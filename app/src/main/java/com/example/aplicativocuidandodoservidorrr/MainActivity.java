73f83fbecfb0f8238166f93acf0415a3a0185cd1
Dion Laranjeira <dionribeiro.rr@gmail.com>
2020-06-01 19:24:21 -0400

    Aula dia 01-06-2020
    Cadastro funcionando no Firebase

--- app/src/main/java/com/example/aplicativocuidandodoservidorrr/MainActivity.java
+++ app/src/main/java/com/example/aplicativocuidandodoservidorrr/MainActivity.java
@@ -4,26 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+    //Pego uma referencia do nosso no Firebase
+    private DatabaseReference meubanco = FirebaseDatabase.getInstance().getReference();
+
     EditText campoCPF;
     EditText campoNome;
     EditText campoDataNasc;
     RadioGroup radioGroupSexo;
     Spinner spinnerRacas;
-
+    RadioButton masculino;
+    RadioButton feminino;
     Button btnCadastrar;
-
+    String sexo = "Masculino";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        meubanco.child("teste").setValue("olá mundo Firebase!");
+        meubanco.child("teste").child("outro nó").child("mais outro nó").setValue("Exemplos nó!");
 
         campoCPF = findViewById(R.id.editTextCPF);
         campoNome = findViewById(R.id.editTextNomeCompleto);
@@ -32,6 +44,17 @@ public class MainActivity extends AppCompatActivity {
         spinnerRacas = findViewById(R.id.spinnerRaca);
         btnCadastrar = findViewById(R.id.buttonCadastrar);
 
+        radioGroupSexo.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId == R.id.radioButtonMasculino){
+                    sexo = "Masculino";
+                }else{
+                    sexo = "Feminino";
+                }
+            }
+        });
+
 
         btnCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,12 +62,27 @@ public class MainActivity extends AppCompatActivity {
                 String cpf = pegaDadosServidor(campoCPF);
                 String nome = pegaDadosServidor(campoNome);
                 String dataNasc = pegaDadosServidor(campoDataNasc);
+
+                String raca;
+
+                String racas [] = {"Parda","Branca","Negra","Indígena"};
+                int posicao = spinnerRacas.getSelectedItemPosition();
+                String racaSelecionada = racas[posicao];
+
+                Servidor novoServidor = new Servidor();
+
+                novoServidor.setCpf(cpf);
+                novoServidor.setNomeCompleto(nome);
+                novoServidor.setDataNasc(dataNasc);
+                novoServidor.setRaca(racaSelecionada);
+                novoServidor.setSexo(sexo);
+
+                meubanco.child(novoServidor.getCpf()).setValue(novoServidor);
+
             }
         });
 
     }
-
-
 
     public String pegaDadosServidor(EditText campo){
         String valorDigitado = null;
---
+++ app/src/main/java/com/example/aplicativocuidandodoservidorrr/Servidor.java
@@ -0,0 +1,50 @@
+package com.example.aplicativocuidandodoservidorrr;
+
+public class Servidor {
+
+private String cpf;
+    private String nomeCompleto;
+    private String dataNasc;
+    private String sexo;
+    private String raca;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getDataNasc() {
+        return dataNasc;
+    }
+
+    public void setDataNasc(String dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+}
